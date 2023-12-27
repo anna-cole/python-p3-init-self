7010fc3ba8f4052d7c97f7dd401fc82ede2149c4
Anna Raptidou Cole <acrrj123@gmail.com>
2023-12-27 14:54:42 -0600

    Lab done

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed='Mutt'):
+        self.name = name
+        self.breed = breed
+        #print(self)
+
+# a = Dog('Ana')
+# a = Dog('Stuart', 'Poodle')
+# print(a.name)
+# print(a.breed)
+
+        
+# the function inside the class, in this case __init__, is called instance method. The self keyword is the object itself, a variable name you can choose. The attributes of the Dog class are name and breed.
